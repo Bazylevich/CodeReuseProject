788415925a03952d70120aabb37d989cb60b3fac
Bazylevich <victoria.bazylevich@gmail.com>
2022-07-05 21:05:00 +0300

    CodeReuse project

--- src/by/itstep/vika/javalessons/lesson22/controller/Task02.java
+++ src/by/itstep/vika/javalessons/lesson22/controller/Task02.java
@@ -1,2 +1,16 @@
-package by.itstep.vika.javalessons.lesson22.controller;public class Task02 {
+package by.itstep.vika.javalessons.lesson22.controller;
+
+import by.itstep.vika.javalessons.lesson22.model.entity.Student;
+public class Task02 {
+    public static void main(String[] args) {
+        Student student = new Student("Alex", 20, 4, true);
+        student.setMark(8);
+        System.out.println(student.getInfo());
+
+        student.setMark(20);
+        System.out.println(student.getInfo());
+
+        student.setMark(-50);
+        System.out.println(student.getInfo());
+    }
 }
--- src/by/itstep/vika/javalessons/lesson22/model/entity/Worker.java
+++ src/by/itstep/vika/javalessons/lesson22/model/entity/Worker.java
@@ -1,2 +1,27 @@
-package by.itstep.vika.javalessons.lesson22.model.entity;public class Worker {
+package by.itstep.vika.javalessons.lesson22.model.entity;
+
+public class Worker {
+    private String name;
+    private int age;
+    private double salary;
+    private boolean alive;
+
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Worker(String name, int age, double salary, boolean alive) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+        this.alive = alive;
+    }
+    public String getInfo(){
+        return "Worker - " + name + ": age = " + age + ", salary" + salary + ", is alive = " + (alive ? "yes" :"no");
+    }
 }
